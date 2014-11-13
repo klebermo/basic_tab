e17b4ce005f2346225a74fab40e992b7756634b3
Kleber Mota <kelbermo@gmail.com>
2014-11-13 13:36:34 -0300

    Updating pom.xml (fixing support-v4 entry

--- src/main/java/org/hello/MainActivity.java
+++ src/main/java/org/hello/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
